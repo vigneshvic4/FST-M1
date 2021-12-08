1f14085e65ab80da26285a8e08ea61f611aea56e
Vignesh Jayaraman <64395224+vigneshvic4@users.noreply.github.com>
2021-12-08 05:51:32 +0530

    Add files via upload

---
+++ Java/Activities/FileUtils.java
@@ -0,0 +1,27 @@
+package org.apache.commons.io;
+
+import java.io.File;
+
+public class FileUtils {
+
+	public static String readFileToString(File fileUtil, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void copyFileToDirectory(File file, File destDir) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public static File getFile(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static File getFile(File destDir, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
