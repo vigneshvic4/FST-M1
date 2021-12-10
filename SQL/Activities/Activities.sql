246445da4f73e32e8c080f529f70b0c6b35d411b
Vignesh Jayaraman <64395224+vigneshvic4@users.noreply.github.com>
2021-12-10 11:40:25 +0530

    Update Activities.sql

--- SQL/Activities/Activities.sql
+++ SQL/Activities/Activities.sql
@@ -169,3 +169,49 @@ insert into customers values
 (3005, 'Graham Zusi', 'California', 200, 5002), (3008, 'Julian Green', 'London', 300, 5002),
 (3004, 'Fabian Johnson', 'Paris', 300, 5006), (3009, 'Geoff Cameron', 'Berlin', 100, 5003),
 (3003, 'Jozy Altidor', 'Moscow', 200, 5007), (3001, 'Brad Guzan', 'London', 300, 5005);
+
+-- Write an SQL statement to find the list of customers who appointed a salesman for their jobs who gets a commission from the company is more than 12%.
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a 
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id 
+WHERE b.commission>12;
+
+-- Write an SQL statement to find the details of a order i.e. order number, order date, amount of order, which customer gives the order and which salesman works for that customer and commission rate he gets for an order.
+SELECT a.order_no, a.order_date, a.purchase_amount, b.customer_name AS "Customer Name", b.grade, c.name AS "Salesman", c.commission FROM orders a 
+INNER JOIN customers b ON a.customer_id=b.customer_id 
+INNER JOIN salesman c ON a.salesman_id=c.salesman_id;
+
+-- Write a query to find all the orders issued against the salesman who may works for customer whose id is 3007.
+SELECT * FROM orders
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+-- Write a query to find all orders attributed to a salesman in New York.
+SELECT * FROM orders
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+-- Write a query to count the customers with grades above New York's average.
+SELECT grade, COUNT(*) FROM customers
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+-- Write a query to extract the data from the orders table for those salesman who earned the maximum commission
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders
+WHERE salesman_id IN( SELECT salesman_id FROM salesman
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
+-- Write a query that produces the name and number of each salesman and each customer with more than one current order. Put the results in alphabetical order
+SELECT customer_id, customer_name FROM customers a
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.customer_id = b.customer_id)
+UNION
+SELECT salesman_id, name FROM salesman a
+WHERE 1<(SELECT COUNT(*) FROM orders b WHERE a.salesman_id = b.salesman_id)
+ORDER BY customer_name;
+
+-- Write a query to make a report of which salesman produce the largest and smallest orders on each date.
+SELECT a.salesman_id, name, order_no, 'highest on', order_date FROM salesman a, orders b
+WHERE a.salesman_id=b.salesman_id
+AND b.purchase_amount=(SELECT MAX(purchase_amount) FROM orders c WHERE c.order_date = b.order_date)
+UNION
+SELECT a.salesman_id, name, order_no, 'lowest on', order_date FROM salesman a, orders b
+WHERE a.salesman_id=b.salesman_id
+AND b.purchase_amount=(SELECT MIN(purchase_amount) FROM orders c WHERE c.order_date = b.order_date);
+
+
