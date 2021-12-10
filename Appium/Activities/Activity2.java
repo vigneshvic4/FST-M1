b98d3424d27c13a0aed46451c40d7e25144f9611
Vignesh Jayaraman <64395224+vigneshvic4@users.noreply.github.com>
2021-12-10 23:33:14 +0530

    Update Activity2.java

--- Appium/Activities/Activity2.java
+++ Appium/Activities/Activity2.java
@@ -1,8 +1,19 @@
-package Appium;
+package com.ibm.appiumTraining.FST_Appium_Project;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 
 public class Activity2 {
     WebDriverWait wait;
