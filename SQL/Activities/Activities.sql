319fb2aaed5752a612ef18aaff9e155fa42ed009
Vignesh Jayaraman <64395224+vigneshvic4@users.noreply.github.com>
2021-12-10 11:36:30 +0530

    Update Activities.sql

--- SQL/Activities/Activities.sql
+++ SQL/Activities/Activities.sql
@@ -156,3 +156,16 @@ WHERE order_date=To_DATE('2012/08/17', 'YYYY/MM/DD') GROUP BY salesman_id, order
 SELECT customer_id, order_date, MAX(purchase_amount) AS "Max Amount" FROM orders
 GROUP BY customer_id, order_date
 HAVING MAX(purchase_amount) IN(2030, 3450, 5760, 6000);
+
+
+-- Create the customers table
+create table customers (
+    customer_id int primary key, customer_name varchar(32),
+    city varchar(20), grade int, salesman_id int);
+
+-- Insert values into it
+insert into customers values 
+(3002, 'Nick Rimando', 'New York', 100, 5001), (3007, 'Brad Davis', 'New York', 200, 5001),
+(3005, 'Graham Zusi', 'California', 200, 5002), (3008, 'Julian Green', 'London', 300, 5002),
+(3004, 'Fabian Johnson', 'Paris', 300, 5006), (3009, 'Geoff Cameron', 'Berlin', 100, 5003),
+(3003, 'Jozy Altidor', 'Moscow', 200, 5007), (3001, 'Brad Guzan', 'London', 300, 5005);
